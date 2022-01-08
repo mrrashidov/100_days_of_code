b223ea35aae74012677390455fcdb393ef6bae0b
Shoxrux Rashidov <dev.rashidov@gmail.com>
2022-01-08 20:02:17 +0300

    Added Query me type
    Object in Object tiplari o`rganildi

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const {graphql, buildSchema} = require("graphql");
 const schema = buildSchema(`
+  
   type Product {
     id: Int!
     name:String
   }
+  
+  type UserName {
+    nick: String
+    first:String
+    last:String
+  }
+
+  type User {
+    id: ID
+    avatar:String
+    name: UserName
+  }
+  
+  type UserProducts {
+    count: Int
+    products:[Product!]
+  }
+  
+  type UserResponse {
+    id: ID
+    user: User
+    items:UserProducts
+  }
+  
   type Query {
     hello: String!
+    me: UserResponse!
     product(id:Int!): Product! # Object | null
     products(page: Int): [Product!]
   }
@@ -13,6 +39,26 @@ const schema = buildSchema(`
 
 const resolversRoot = {
   hello: () => ('Hello World'),
+  me:()=> ({
+    id: 1,
+    user: {
+      id: 1,
+      name:{
+        nick:'john',
+        first:'John',
+        last:'Doe'
+      }
+    },
+    items:{
+      count: 99,
+      products:[
+        {
+          id:1,
+          name:"olma"
+        }
+      ]
+    }
+  }),
   product: ({id}) => ({
     id: id,
     name: "product1",
@@ -29,4 +75,4 @@ const resolversRoot = {
   ])
     }
 ;
-graphql(schema, `{ products { id name } }`, resolversRoot).then(response => console.log(response.data));
+graphql(schema, `{ me { id user { id name { nick first last } } items { count products{ id name } } } }`, resolversRoot).then(response => console.log(response.data));
