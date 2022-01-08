adf0d29191b9c0d11c82a7f51f058326696c1796
zsaidov1988 <zsaidov1988@gmail.com>
2022-01-09 02:35:00 +0500

    Initial commit

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,140 +1,377 @@
-const {graphql, buildSchema} = require("graphql");
+const { graphql, buildSchema } = require("graphql");
 const schema = buildSchema(`
   
-  input CreateProductInput {
-    name:String!
-    price: Int!
+  input CreateUserInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  input UpdateUserInput {
+    id: ID!
+    name: String
+    email: String
+    password: String
+  }
+
+  input DeleteUserInput {
+    id: ID!
   }
   
-  input UpdateProductInput {
-    id:ID!
-    name:String
-    price: Int
+  input CreateTodoInput {
+    body: String!
+    status: Boolean!
+    userId: ID!
+  }
+
+  input UpdateTodoInput {
+    id: ID!
+    body: String
+    status: Boolean
+  }
+
+  input DeleteTodoInput {
+    id: ID!
   }
   
-  type Product {
-    id: Int!
-    name:String
-    price:Float
-  }
-  
-  type UserName {
-    nick: String
-    first:String
-    last:String
+  type Todo {
+    id: ID!
+    body: String!
+    status: Boolean
+    userId: ID!
   }
 
   type User {
-    id: ID
-    avatar:String
-    name: UserName
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
   }
-  
-  type UserProducts {
-    count: Int
-    products:[Product!]
+
+  type DeletedUser {
+    name: String!
   }
-  
-  type UserResponse {
-    id: ID
-    user: User
-    items:UserProducts
+
+  type DeletedTodo {
+    id: ID!
   }
   
   type Query {
-    hello: String!
-    me: UserResponse!
-    product(id:Int!): Product! # Object | null
-    products(page_size: Int page_number: Int): [Product!]
+    me(id: ID!): User!
+    todo(id: ID!): Todo
+    todos: [Todo!]
   }
+
   type Mutation {
-    createProduct(input: CreateProductInput!): Product!
-    updateProduct(input: UpdateProductInput!): Product!
+    createUser(input:CreateUserInput!):User!
+    updateUser(input:UpdateUserInput!):User!
+    deleteUser(input:DeleteUserInput!):DeletedUser!
+    createTodo(input:CreateTodoInput!):Todo!
+    updateTodo(input:UpdateTodoInput!):Todo!
+    deleteTodo(input:DeleteTodoInput!):DeletedTodo!
   }
 `);
 
-const data = {
-    user: {
-        id: 1,
-        avatar: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/12/1264ca0c90886ff9a6ab015cfc286b08b8657874_full.jpg',
-        firstName: 'John',
-        lastName: 'Doe',
-        nick: 'john'
+let data = {
+  users: [
+    {
+      id: 1,
+      name: "Ali",
+      email: "ali@mail.ru",
+      password: "12345",
     },
-    products: [
-        {
-            id: 1,
-            name: "First Product",
-            price: 10.99
-        },
-        {
-            id: 2,
-            name: "Second Product",
-            price: 3.99
-        },
-        {
-            id: 3,
-            name: "Third Product",
-            price: 2.99
-        },
-        {
-            id: 4,
-            name: "Fourth Product",
-            price: 1.99
-        }
-    ],
-    user_products: [
-        {
-            user_id: 1,
-            product_id: 1
-        },
-        {
-            user_id: 1,
-            product_id: 4
-        }
-    ]
-}
-
-const paginate = (array, page_size, page_number) => (array.slice(page_number * page_size, page_number * page_size + page_size))
-const resolversRoot = {
-    hello: () => ('Hello World'),
-    me: () => ({
-        id: data.user.id,
-        user: {
-            id: data.user.id,
-            avatar: data.user.avatar,
-            name: {
-                nick: data.user.nick,
-                first: data.user.firstName,
-                last: data.user.lastName
-            }
-        },
-        items: {
-            count: data.user_products.length,
-            products: data.user_products.map(up => data.products.find(product => product.id === up.product_id))
-        }
-    }),
-    product: ({id}) => (data.products.find(product => product.id === parseInt(id))),
-    products: ({page_size, page_number}) => (paginate(data.products, parseInt(page_size), parseInt(page_number) - 1)),
-    createProduct: ({input}) => {
-      const newData = {
-        id: data.products.length + 1,
-        name: input.name,
-        price: input.price
-      }
-      data.products.push(newData)
-      return newData
+    {
+      id: 2,
+      name: "Zafar",
+      email: "zafar@mail.ru",
+      password: "12345",
     },
-    updateProduct: ({input}) => {
-        const findIndex = data.products.findIndex((product => product.id === parseInt(input.id)));
-        data.products[findIndex].name = input.name
-        return data.products.find(product => product.id === parseInt(input.id))
-
+    {
+      id: 3,
+      name: "Vali",
+      email: "vali@mail.ru",
+      password: "12345",
     },
+  ],
+  todos: [
+    {
+      id: 1,
+      body: "Read the book",
+      status: true,
+      userId: 2,
+    },
+    {
+      id: 2,
+      body: "Do homeworks",
+      status: true,
+      userId: 1,
+    },
+    {
+      id: 3,
+      body: "Have a rest",
+      status: true,
+      userId: 1,
+    },
+    {
+      id: 4,
+      body: "Learn English",
+      status: true,
+      userId: 3,
+    },
+    {
+      id: 5,
+      body: "Graphql",
+      status: true,
+      userId: 2,
+    },
+  ],
 };
-//graphql(schema, `{ hello }`, resolversRoot).then(response => console.log(response));
-//graphql(schema, `{ product(id:1) { id name } }`, resolversRoot).then(response => console.log(response));
-//graphql(schema, `{ products(page_size:11 page_number:1) { id name } }`, resolversRoot).then(response => console.log(response.data));
-graphql(schema, `{ me { id user { id name { nick first last } avatar } items { count products { id name price } } } }`, resolversRoot).then(response => console.log(response));
-//graphql(schema, `mutation{ createProduct(input:{ name: "Test", price: 21}) { id name price } }`, resolversRoot).then(response => console.log(response));
-//graphql(schema, `mutation{ updateProduct(input:{ id:1, name: "Test (updated)"}) { id name } }`, resolversRoot).then(response => console.log(response));
+
+const resolversRoot = {
+  me: ({ id }) => ({
+    id: id,
+    name: data.users[id].name,
+    email: data.users[id].email,
+  }),
+
+  todo: ({ id }) => ({
+    id: id,
+    body: data.todos[id].body,
+    status: data.todos[id].status,
+    userId: data.todos[id].userId,
+  }),
+
+  todos: () => data.todos,
+
+  createUser: ({ input }) => {
+    const newUser = {
+      id: data.users[data.users.length - 1].id + 1,
+      name: input.name,
+      email: input.email,
+      password: input.password,
+    };
+    data.users.push(newUser);
+    return newUser;
+  },
+
+  updateUser: ({ input }) => {
+    const index = data.users.findIndex(
+      (user) => user.id === parseInt(input.id)
+    );
+    if (!index) return "User not found";
+    if (input.name) {
+      data.users[index].name = input.name;
+    }
+    if (input.email) {
+      data.users[index].email = input.email;
+    }
+    if (input.password) {
+      data.users[index].password = input.password;
+    }
+    return data.users[index];
+  },
+
+  deleteUser: ({ input }) => {
+    const index = data.users.findIndex(
+      (user) => user.id === parseInt(input.id)
+    );
+    if (index < 0) return { name: "User not found" };
+    const name = data.users[index].name;
+    data.users.splice(index, 1);
+
+    return {
+      name,
+    };
+  },
+
+  createTodo: ({ input }) => {
+    const newTodo = {
+      id: data.todos[data.todos.length - 1].id + 1,
+      body: input.body,
+      status: input.status || false,
+      userId: input.userId,
+    };
+    data.todos.push(newTodo);
+    return newTodo;
+  },
+
+  updateTodo: ({ input }) => {
+    const index = data.todos.findIndex(
+      (todo) => todo.id === parseInt(input.id)
+    );
+    if (!index) return "Todo not found";
+    if (input.body) {
+      data.todos[index].body = input.body;
+    }
+    if (input.status !== null) {
+      data.todos[index].status = input.status;
+    }
+    return data.todos[index];
+  },
+
+  deleteTodo: ({ input }) => {
+    const index = data.todos.findIndex(
+      (todo) => todo.id === parseInt(input.id)
+    );
+    if (index < 0) return { id: "Not found" };
+    const id = data.todos[index].id;
+    data.todos.splice(index, 1);
+
+    return {
+      id,
+    };
+  },
+};
+
+// TODO
+
+// graphql(
+//   schema,
+//   `
+//     {
+//       todo(id: 1) {
+//         id
+//         body
+//         status
+//         userId
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// TODOS
+
+// graphql(
+//   schema,
+//   `
+//     {
+//       todos {
+//         id
+//         body
+//         status
+//         userId
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// ME
+
+// graphql(
+//   schema,
+//   `
+//     {
+//       me(id: 1) {
+//         id
+//         name
+//         email
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// UPDATE USER
+
+// graphql(
+//   schema,
+//   `
+//     mutation {
+//       updateUser(
+//         input: {
+//           id: 2
+//           name: "user2"
+//           email: "user2@mail.ru"
+//           password: "12345"
+//         }
+//       ) {
+//         id
+//         name
+//         email
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// CREATE USER
+
+// graphql(
+//   schema,
+//   `
+//     mutation {
+//       createUser(
+//         input: { name: "user3", email: "user3@mail.ru", password: "12345" }
+//       ) {
+//         id
+//         name
+//         email
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// DELETE USER
+
+// graphql(
+//   schema,
+//   `
+//     mutation {
+//       deleteUser(input: { id: 1 }) {
+//         name
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// UPDATE TODO
+
+// graphql(
+//   schema,
+//   `
+//     mutation {
+//       updateTodo(input: { id: 3, body: "Updated body", status: false }) {
+//         id
+//         body
+//         status
+//         userId
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// CREATE TODO
+
+// graphql(
+//   schema,
+//   `
+//     mutation {
+//       createTodo(input: { body: "Body of todo", status: false, userId: 2 }) {
+//         id
+//         body
+//         userId
+//         status
+//       }
+//     }
+//   `,
+//   resolversRoot
+// ).then((response) => console.log(response));
+
+// DELETE TODO
+
+graphql(
+  schema,
+  `
+    mutation {
+      deleteTodo(input: { id: 3 }) {
+        id
+      }
+    }
+  `,
+  resolversRoot
+).then((response) => console.log(response));
