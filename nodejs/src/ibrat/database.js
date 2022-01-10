9967b393f9a6b3f31854cec3e11310f39c1672a0
Ibrat <ibrat@abutech.uz>
2022-01-10 15:06:58 +0500

    add database

---
+++ nodejs/src/ibrat/database.js
@@ -0,0 +1,30 @@
+module.exports = {
+  users: [
+    {
+      id: 1,
+      firstName: "Ibrat",
+      lastName: "Tillaberdiyev",
+      age: 20,
+    },
+    {
+      id: 2,
+      firstName: "Murod",
+      lastName: "Islomov",
+      age: 18,
+    },
+  ],
+  todos: [
+    {
+      id: 1,
+      title: "Users Service",
+      description: "Users serviceda getUserById'ni yozish kerak kerak.",
+      userId: 2,
+    },
+    {
+      id: 2,
+      title: "Products Service",
+      description: "Products serviceda getCountOfProducts'ni yozish kerak.",
+      userId: 1,
+    },
+  ],
+};
