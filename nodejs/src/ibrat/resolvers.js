991c44bd08048000bca56c4ed8e8817ddee568c2
Ibrat <ibrat@abutech.uz>
2022-01-10 15:28:45 +0500

    todo resolvers

--- nodejs/src/ibrat/resolvers.js
+++ nodejs/src/ibrat/resolvers.js
@@ -41,8 +41,52 @@ const deleteUser = (args) => {
   return userIndex;
 };
 
+const createTodo = (args) => {
+  const todo = {
+    id: database.todos.length + 1,
+    title: args.title,
+    description: args.description,
+    userId: args.userId,
+  };
+
+  database.todos.push(todo);
+
+  return todo;
+};
+
+const updateTodo = (args) => {
+  const todoIndex = database.todos.findIndex((todo) => args.id == todo.id);
+
+  if (todoIndex < 0) {
+    return `This todo doest not exists`;
+  }
+
+  database.todos[todoIndex] = {
+    title: args.title,
+    description: args.description,
+    userId: args.userId,
+  };
+
+  return database.todos[todoIndex];
+};
+
+const deleteTodo = (args) => {
+  const todoIndex = database.todos.findIndex((todo) => args.id == todo.id);
+
+  if (todoIndex < 0) {
+    return `This todo doest not exists`;
+  }
+
+  database.todos.splice(todoIndex, 1);
+
+  return todoIndex;
+};
+
 module.exports = {
   createUser,
   updateUser,
   deleteUser,
+  createTodo,
+  updateTodo,
+  deleteTodo,
 };
