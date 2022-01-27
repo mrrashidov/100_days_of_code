0e64b0ea3770c998b5663503a533f16c1c1d32e3
zsaidov1988 <zsaidov1988@gmail.com>
2022-01-27 08:55:58 +0500

    Add validator for TODO input

--- home-tasks/zafar/backend/src/services/todo/validators/index.js
+++ home-tasks/zafar/backend/src/services/todo/validators/index.js
@@ -1,4 +1,5 @@
 const yup = require("yup");
+const { number } = require("yup/lib/locale");
 module.exports = {
   createCategory: yup.object({
     name: yup.string().required().min(3).max(20),
@@ -46,5 +47,23 @@ module.exports = {
       ]),
     description: yup.string().max(50),
     status: yup.string().oneOf(["active", "passive", "pending"]),
+  }),
+  createTodo: yup.object({
+    userId: yup.number().required(),
+    name: yup.string().required().min(3).max(20),
+    description: yup.string().required().max(50),
+    type: yup.boolean(),
+    status: yup.string().oneOf(["active", "passive", "pending"]),
+    tagId: yup.array(number).required(),
+    categoryId: yup.array(number).required(),
+  }),
+  updateTodo: yup.object({
+    id: yup.number().required(),
+    name: yup.string().min(3).max(20),
+    description: yup.string().max(50),
+    type: yup.boolean(),
+    status: yup.string().oneOf(["active", "passive", "pending"]),
+    tagId: yup.array(number),
+    categoryId: yup.array(number),
   }),
 };
