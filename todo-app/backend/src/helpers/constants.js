53a1a1ad3dc1d1aacf7fa91041c7dd78bd3ce93a
MrRashidov <dev.rashidov@gmail.com>
2022-01-31 19:53:58 +0300

    added object converter function

--- todo-app/backend/src/helpers/constants.js
+++ todo-app/backend/src/helpers/constants.js
@@ -1,12 +1,15 @@
 module.exports = {
-    model_type: {
-        user: 1,
-        team: 2
-    },
-    status: {
-        active: 1,
-        passive: 2,
-        pending: 3,
-        draft: 4,
-    }
-}
+  model_type: {
+    user: 1,
+    team: 2,
+  },
+  status: {
+    active: 1,
+    passive: 2,
+    pending: 3,
+    draft: 4,
+  },
+  getKeyByValue: function (object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  },
+};
